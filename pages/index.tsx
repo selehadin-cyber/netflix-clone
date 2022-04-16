aedc4067b22658443bb94b40292e9bbb30a8b33e
selehadin-cyber <sele4416@gmail.com>
2022-04-16 11:46:48 +0300

    main page done

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,7 +29,7 @@ const Home = ({
 }: Props) => {
   console.log(netflixOriginals)
   return (
-    <div className="relative h-screen bg-gradient-to-b from-gray-900/10 to-[#010511] lg:h-[140vh]">
+    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
@@ -39,7 +39,7 @@ const Home = ({
       <main className='pl-4 pb-24 lg:space-y-24 lg:pl-16'>
         {/* Banner */}
         <Banner netflixOriginals={netflixOriginals}/>
-        <section>
+        <section className='md:space-y-24'>
           {/* Rows */}
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
@@ -49,8 +49,8 @@ const Home = ({
 
           <Row title="Comedies" movies={comedyMovies} />
           <Row title="Scary Movies" movies={horrorMovies} />
-          <Row title="Romance Movies" movies={romanceMovies} />
-          <Row title="Documentaries" movies={documentaries} />
+          {/* <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} /> */}
         </section>
       </main>
       {/* Modal */}
