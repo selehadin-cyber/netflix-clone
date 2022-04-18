882a75a5c3667c4bd436cb7b1ffe7245b53ee27e
selehadin-cyber <sele4416@gmail.com>
2022-04-18 15:12:04 +0300

    Modal added ✨

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,32 +29,35 @@ const Home = ({
   horrorMovies,
   romanceMovies,
   topRated,
-  trendingNow
+  trendingNow,
 }: Props) => {
   //console.log(netflixOriginals)
-  const {loading} = useAuth();
+  const { loading } = useAuth()
   const showModal = useRecoilValue(modalState)
 
   if (loading) return null
 
   return (
-    <div className="relative h-screen bg-gradient-to-b lg:h-[140vh]">
+    <div
+      className={`relative h-screen bg-gradient-to-b lg:h-[140vh] ${
+        showModal && '!h-screen overflow-hidden'
+      }`}
+    >
       <Head>
         <title>Home - Netflix</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Header */}
       <Header />
-      <main className='pl-4 pb-24 lg:space-y-24 lg:pl-16'>
+      <main className="pl-4 pb-24 lg:space-y-24 lg:pl-16">
         {/* Banner */}
-        <Banner netflixOriginals={netflixOriginals}/>
-        <section className='md:space-y-24'>
+        <Banner netflixOriginals={netflixOriginals} />
+        <section className="md:space-y-24">
           {/* Rows */}
           <Row title="Trending Now" movies={trendingNow} />
           <Row title="Top Rated" movies={topRated} />
           <Row title="Action Thrillers" movies={actionMovies} />
           {/* My List */}
-          
 
           <Row title="Comedies" movies={comedyMovies} />
           <Row title="Scary Movies" movies={horrorMovies} />
